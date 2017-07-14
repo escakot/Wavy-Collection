46dab697df60c92467112dce2f7d332300ea3c47
escakot <ec@neorennie.com>
2017-07-13 21:46:28 -0400

    Initial Commit

---
+++ Wavy Collection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wavy Collection
+//
+//  Created by Errol Cheong on 2017-07-13.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Wavy Collection/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Wavy Collection
+//
+//  Created by Errol Cheong on 2017-07-13.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
